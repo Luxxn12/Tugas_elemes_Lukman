501f46bd75592f45762c114681116b2cb1f9cae0
lukman <lukman@lukmans-MacBook-Air.local>
2023-11-11 22:19:59 +0800

    style : update modules
    Deskripsi:
    - update page app
    - create view layout
    - create new project
    
    No Issue

---
+++ src/modules/_global/front/view/view_layout.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@mantine/core';
+import React from 'react';
+
+export default function ViewLayout() {
+  return (
+    <>
+    <Text>SETUP PROJECT ELEMES</Text>
+    </>
+  );
+}
+
